55462c5eedcb340116fc80454f39f342e00f94f0
Gokari-123 <mahaboobgokari@gmail.com>
2024-12-04 12:26:09 +0530

    Add files via upload

---
+++ Online Courier Service/Online-Courier-Service/src/main/java/Online/Courier/Service/OnlineCourierServiceApplication.java
@@ -0,0 +1,13 @@
+package Online.Courier.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourierServiceApplication.class, args);
+	}
+
+}
---
+++ Online Courier Service/Online-Courier-Service/src/test/java/Online/Courier/Service/OnlineCourierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Online.Courier.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
